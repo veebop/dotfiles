8100a7420abbdc9e2f6266a906483ce6b37bdbe8
Eve Jensen <evesjensen@gmail.com>
2023-09-27 00:49:49 -0600

    Fixed which-key preset z

--- dot_config/lvim/lua/private_user/private_options.lua
+++ dot_config/lvim/lua/private_user/private_options.lua
@@ -4,6 +4,27 @@ lvim.colorscheme = "kanagawa"
 -- Map LocalLeader
 vim.g.maplocalleader = "," -- Comma seems to be the most common
 
+-- Adjust what is shown with which-key.nvim
+lvim.builtin.which_key.setup.plugins = {
+  marks = true,     -- shows a list of your marks on ' and `
+  registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+  -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+  -- No actual key bindings are created
+  spelling = {
+    enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+    suggestions = 20, -- how many suggestions should be shown in the list?
+  },
+  presets = {
+    operators = true,    -- adds help for operators like d, y, ...
+    motions = true,      -- adds help for motions
+    text_objects = true, -- help for text objects triggered after entering an operator
+    windows = true,      -- default bindings on <c-w>
+    nav = true,          -- misc bindings to work with windows
+    z = true,            -- bindings for folds, spelling and others prefixed with z
+    g = true,            -- bindings for prefixed with g
+  },
+}
+
 -- Code folding by treesitter
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
@@ -54,27 +75,6 @@ lvim.keys.normal_mode["<leader>lR"] = ":IncRename "
 -- Code formatters with null-ls (rip)
 lvim.lsp.null_ls.formatters.setup {
   { name = "rustfmt" },
-}
-
--- Adjust what is shown with which-key.nvim
-lvim.builtin.which_key.setup.plugins = {
-  marks = true,     -- shows a list of your marks on ' and `
-  registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-  -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-  -- No actual key bindings are created
-  spelling = {
-    enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-    suggestions = 20, -- how many suggestions should be shown in the list?
-  },
-  presets = {
-    operators = true,    -- adds help for operators like d, y, ...
-    motions = true,      -- adds help for motions
-    text_objects = true, -- help for text objects triggered after entering an operator
-    windows = true,      -- default bindings on <c-w>
-    nav = true,          -- misc bindings to work with windows
-    z = true,            -- bindings for folds, spelling and others prefixed with z
-    g = true,            -- bindings for prefixed with g
-  },
 }
 
 -- Enable completion for the command line
