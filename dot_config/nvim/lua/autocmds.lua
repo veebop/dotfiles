8c6575af4325077e2a501c44ced36658be09418e
Eve Jensen <evesjensen@gmail.com>
2023-10-03 18:50:46 -0600

    nvim-tree now opens when opening a directory

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -14,3 +14,17 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   desc = "Auto format",
   command = "lua vim.lsp.buf.format({ async = false})",
 })
+
+-- Event emitted when a directory is opened - Useful for nvim-tree
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("_dir_opened", {}),
+  nested = true,
+  callback = function(args)
+    local bufname = vim.api.nvim_buf_get_name(args.buf)
+    if require("utils").is_directory(bufname) then
+      vim.api.nvim_del_augroup_by_name "_dir_opened"
+      vim.cmd "do User DirOpened"
+      vim.api.nvim_exec_autocmds(args.event, { buffer = args.buf, data = args.data })
+    end
+  end,
+})
--- dot_config/nvim/lua/utils.lua
+++ dot_config/nvim/lua/utils.lua
@@ -1,25 +1,30 @@
-local M = {
-  -- string padding (I stole this code hehe https://github.com/sho-87/dotfiles/blob/master/nvim/lua/utils.lua
-  pad_string = function(str, len, align, num_nerd_font)
-    -- Lua doesn't handle nerd font characters well, so we have to manually remove extra counted characters
-    local str_len = #str - ((num_nerd_font or 0) * 2)
-    if str_len >= len then
-      return str
-    end
+local M = {}
 
-    local pad_len = len - str_len
-    local pad = string.rep(" ", pad_len)
-
-    if align == "left" then
-      return str .. pad
-    elseif align == "right" then
-      return pad .. str
-    elseif align == "center" then
-      local left_pad = math.floor(pad_len / 2)
-      local right_pad = pad_len - left_pad
-      return string.rep(" ", left_pad) .. str .. string.rep(" ", right_pad)
-    end
+-- string padding (I stole this code hehe https://github.com/sho-87/dotfiles/blob/master/nvim/lua/utils.lua
+function M.pad_string(str, len, align, num_nerd_font)
+  -- Lua doesn't handle nerd font characters well, so we have to manually remove extra counted characters
+  local str_len = #str - ((num_nerd_font or 0) * 2)
+  if str_len >= len then
+    return str
   end
-}
+
+  local pad_len = len - str_len
+  local pad = string.rep(" ", pad_len)
+
+  if align == "left" then
+    return str .. pad
+  elseif align == "right" then
+    return pad .. str
+  elseif align == "center" then
+    local left_pad = math.floor(pad_len / 2)
+    local right_pad = pad_len - left_pad
+    return string.rep(" ", left_pad) .. str .. string.rep(" ", right_pad)
+  end
+end
+
+function M.is_directory(path)
+  local stat = vim.loop.fs_stat(path)
+  return stat and stat.type == "directory" or false
+end
 
 return M
