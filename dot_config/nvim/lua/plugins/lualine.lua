b5f9e348f602a6a846c20798bda0dc375b34c24a
Eve Jensen <evesjensen@gmail.com>
2023-10-17 22:34:33 -0600

    Updated lualine

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,11 +1,23 @@
 local M = {}
 
+local function diff_source()
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return {
+      added = gitsigns.added,
+      modified = gitsigns.changed,
+      removed = gitsigns.removed
+    }
+  end
+end
+
 function M.setup()
   require('lualine').setup({
     options = {
       disabled_filetypes = { "alpha" },
     },
     sections = {
+      lualine_b = { { 'b:gitsigns_head', icon = '' }, { 'diff', diff = diff_source() }, 'diagnostics', },
       lualine_c = {
         {
           'filename',
@@ -29,7 +41,8 @@ function M.setup()
     },
     inactive_winbar = {
       lualine_a = { "filename" }
-    }
+    },
+    extensions = { 'lazy', 'mason', 'nvim-tree', }
   })
 end
 
