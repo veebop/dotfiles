7e3dd34d4a3a74908eb441467d20cc8de7f40651
Eve Jensen <evesjensen@gmail.com>
2023-10-18 01:07:56 -0600

    Big lualine changes Mostly adds support for things through extensions, particularly nvim-dap-ui

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,23 +1,98 @@
 local M = {}
 
-local function diff_source()
-  local gitsigns = vim.b.gitsigns_status_dict
-  if gitsigns then
-    return {
-      added = gitsigns.added,
-      modified = gitsigns.changed,
-      removed = gitsigns.removed
-    }
-  end
-end
-
 function M.setup()
+  local function diff_source()
+    local gitsigns = vim.b.gitsigns_status_dict
+    if gitsigns then
+      return {
+        added = gitsigns.added,
+        modified = gitsigns.changed,
+        removed = gitsigns.removed
+      }
+    end
+  end
+
+  -- My extensions
+  -- Usually based off of built in extensions, but in the winbar instead
+  local nvim_dap_ui = {
+    winbar = {
+      lualine_a = { { 'filename', file_status = false } },
+    },
+    filetypes = {
+      'dapui_console',
+      'dapui_watches',
+      'dapui_stacks',
+      'dapui_breakpoints',
+      'dapui_scopes',
+    },
+  }
+
+  local nvim_dap_ui_repl = {
+    winbar = {
+      lualine_a = { "string.format('DAP Repl')" },
+      lualine_b = {
+        { "string.format(' ')", on_click = require('dap').continue },
+        { "string.format(' ')", on_click = require('dap').step_into },
+        { "string.format(' ')", on_click = require('dap').step_over },
+        { "string.format(' ')", on_click = require('dap').step_out },
+        { "string.format(' ')", on_click = require('dap').step_back },
+        { "string.format(' ')", on_click = require('dap').run_last },
+        { "string.format(' ')", on_click = require('dap').terminate },
+        { "string.format(' ')", on_click = require('dap').disconnect },
+      },
+    },
+    inactive_winbar = {
+      lualine_a = { "string.format('DAP Repl ')", },
+      lualine_b = {
+        { "string.format(' ')", on_click = require('dap').continue, separator = " ", },
+        { "string.format(' ')", on_click = require('dap').step_into, separator = " ", },
+        { "string.format(' ')", on_click = require('dap').step_over, separator = " ", },
+        { "string.format(' ')", on_click = require('dap').step_out, separator = " ", },
+        { "string.format(' ')", on_click = require('dap').step_back, separator = " ", },
+        { "string.format(' ')", on_click = require('dap').run_last, separator = " ", },
+        { "string.format(' ')", on_click = require('dap').terminate, separator = " ", },
+        { "string.format(' ')", on_click = require('dap').disconnect, separator = " ", },
+      },
+    },
+    filetypes = {
+      'dap-repl',
+    },
+  }
+
+  local nvim_tree = {
+    winbar = {
+      lualine_a = { "string.format('File Explorer')" }
+    },
+    filetypes = { 'NvimTree' },
+  }
   require('lualine').setup({
     options = {
-      disabled_filetypes = { "alpha" },
+      disabled_filetypes = {
+        'alpha',
+        -- These are handled by extensions
+        winbar = {
+          'mason',
+          'lazy',
+        },
+        statusline = {
+          'NvimTree',
+          'dap-repl',
+          'dapui_console',
+          'dapui_watches',
+          'dapui_stacks',
+          'dapui_breakpoints',
+          'dapui_scopes',
+        },
+      },
     },
+
     sections = {
-      lualine_b = { { 'b:gitsigns_head', icon = '' }, { 'diff', diff = diff_source() }, 'diagnostics', },
+      lualine_a = { 'mode' },
+      lualine_b = {
+        { 'b:gitsigns_head', icon = '' },
+        { 'diff', diff = diff_source() },
+        'diagnostics',
+      },
       lualine_c = {
         {
           'filename',
@@ -32,17 +107,46 @@ function M.setup()
           { 'fileformat', icons_enabled = false, },
           'filetype',
         }
-      }
+      },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' },
     },
+
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {
+        {
+          'filename',
+          path = 1,
+        },
+        { 'diagnostics' },
+        { 'location' },
+      },
+      lualine_y = {},
+      lualine_z = {},
+    },
+
     winbar = {
       lualine_a = { "filename" },
       lualine_b = { "navic" },
       lualine_c = { "searchcount", "selectioncount" },
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {},
     },
+
     inactive_winbar = {
-      lualine_a = { "filename" }
+      lualine_a = { "filename" },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {},
     },
-    extensions = { 'lazy', 'mason', 'nvim-tree', }
+
+    extensions = { 'lazy', 'mason', nvim_tree, nvim_dap_ui, nvim_dap_ui_repl }
   })
 end
 
