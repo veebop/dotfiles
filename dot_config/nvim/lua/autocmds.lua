c49939de2a0f4f0c971dc4c96a369a005a792cdc
Eve Jensen <evesjensen@gmail.com>
2023-10-27 16:14:05 -0600

    Added automatic relative number toggling

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -38,6 +38,30 @@ vim.api.nvim_create_autocmd("BufRead", {
   end,
 })
 
+-- Automatic relative numbers toggle
+local numtoggle = vim.api.nvim_create_augroup("numbertoggle", {})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+  pattern = "*",
+  group = numtoggle,
+  callback = function()
+    if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+      vim.opt.relativenumber = true
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+  pattern = "*",
+  group = numtoggle,
+  callback = function()
+    if vim.o.nu then
+      vim.opt.relativenumber = false
+      vim.cmd "redraw"
+    end
+  end,
+})
+
 ----- BUFFER SPECIFIC KEYBINDINGS -----
 local kb = require("keybindings")
 
