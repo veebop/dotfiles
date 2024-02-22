f906bfaa41524287c362fd3dcc9d38be22973d5b
Eve Jensen <evesjensen@gmail.com>
2024-02-22 15:24:19 -0700

    Neovim: Diff, gitcommit, and git_rebase parsers

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup()
   require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "c", "lua", "rust", "swift", "vim", "vimdoc", "query", "zig" },
+    ensure_installed = { "c", "diff", "gitcommit", "git_rebase", "lua", "rust", "swift", "vim", "vimdoc", "query", "zig" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
