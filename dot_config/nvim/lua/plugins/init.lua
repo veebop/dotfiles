0efacfb8ad7f6cbe85b506d8e6b6c5d2fe2804e7
Eve Jensen <evesjensen@gmail.com>
2024-06-06 18:20:45 -0600

    Neovim: Add tpope/vim-unimpaired

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -270,6 +270,18 @@ require("lazy").setup({
     event = "VeryLazy",
   },
   {
+    -- NOTE: Consider using "idanarye/nvim-impairative" instead
+    "afreakk/unimpaired-which-key.nvim",
+    dependencies = { "tpope/vim-unimpaired" },
+    config = function()
+      local wk = require("which-key")
+      local uwk = require("unimpaired-which-key")
+      wk.register(uwk.normal_mode)
+      wk.register(uwk.normal_and_visual_mode, { mode = { "n", "v" } })
+    end,
+    event = "VeryLazy",
+  },
+  {
     "lewis6991/gitsigns.nvim",
     opts = {},
     event = { "BufRead", "BufNewFile" },
