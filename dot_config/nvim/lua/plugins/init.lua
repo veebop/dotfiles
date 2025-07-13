fe84f3ce5b6ba7eb6687cee4df7fc78fcf4f91be
Eve Jensen <evesjensen@gmail.com>
2025-07-12 23:54:48 -0600

    Neovim: Add dadbod & dadbod-ui

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -409,6 +409,25 @@ require("lazy").setup({
     lazy = true
   },
   ----- LANGUAGE SPECIFIC -----
+  -- SQL
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod',                     lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.db_ui_execute_on_save = 0
+      vim.g.db_ui_user_nvim_notify = 1
+    end,
+  },
   -- Rust
   {
     'mrcjkb/rustaceanvim',
